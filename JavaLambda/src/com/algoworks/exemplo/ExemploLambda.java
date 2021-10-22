16dcd85277879d9b687165c75c37e70ddf792ef3
Arthorius-Pen <utherpendragon@tutanota.com>
2021-10-21 21:10:58 -0300

    Correct body Lambda

--- JavaLambda/src/com/algoworks/exemplo/ExemploLambda.java
+++ JavaLambda/src/com/algoworks/exemplo/ExemploLambda.java
@@ -1,7 +1,6 @@
 package com.algoworks.exemplo;
 
 import java.util.List;
-import java.util.ArrayList;
 
 import com.algoworks.dao.FaturaDAO;
 import com.algoworks.modelo.Fatura;
@@ -9,14 +8,18 @@ import com.algoworks.util.EnviadorEmail;
 
 public class ExemploLambda {
 	public static void main(String[] args) {
-		List<String> nomes= new ArrayList();
-
-		nomes.add("Luis");
-		nomes.add("Fargas");
-		nomes.add("Nobruzera Apelao");
+		List<Fatura> faturasVencidas = new FaturaDAO().buscarFaturasVencidas();
 		
-		nomes.forEach(nome -> {
-			System.out.println(nome);
+		EnviadorEmail enviadoEmail = new EnviadorEmail();
+		
+		/*
+		 * for (Fatura f: faturasVencidas) { enviadoEmail.enviar(f.getEmailDevedor(),
+		 * f.resumo()); }
+		 */
+		
+		faturasVencidas.forEach(f -> {
+			enviadoEmail.enviar(f.getEmailDevedor(), f.resumo());
+			f.setEmailEnviado(true);
 		});
 	}
 }
