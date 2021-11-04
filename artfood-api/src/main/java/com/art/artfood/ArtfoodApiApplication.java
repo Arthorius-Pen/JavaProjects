9ebc54570573f9c7dda4d0ecd42aa758358c0884
Arthorius-Pen <utherpendragon@tutanota.com>
2021-11-04 19:12:41 -0300

    Created clonefood api

---
+++ artfood-api/src/main/java/com/art/artfood/ArtfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.art.artfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtfoodApiApplication.class, args);
+	}
+
+}
