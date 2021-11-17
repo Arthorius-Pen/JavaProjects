04e891c6d4f0f996e16b5f9fa669036e59d021c4
Arthorius-Pen <utherpendragon@tutanota.com>
2021-11-16 21:25:21 -0300

    Added Autowired exemples

--- artfood-api/src/main/java/com/art/artfood/di/notificacao/NotificadorEmail.java
+++ artfood-api/src/main/java/com/art/artfood/di/notificacao/NotificadorEmail.java
@@ -1,29 +1,21 @@
 package com.art.artfood.di.notificacao;
 
+import org.springframework.stereotype.Component;
+
 import com.art.artfood.di.modelo.Cliente;
 
-
+@Component
 public class NotificadorEmail implements Notificador {
-	
-	private boolean caixaAlta;
-	private String hostServidorSmtp;
-	
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
+		
+	public NotificadorEmail() {
 		System.out.println("Componente Spring: Notificador Email");
 	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if(this.caixaAlta) {
-			mensagem = mensagem.toUpperCase();
-		}
 		
- 		System.out.printf("Notificando %s atravéz do email: %s. Usando o SMTP: %s \n->%s\n", 
-				cliente.getNome(), cliente.getEmail(), hostServidorSmtp, mensagem);
+ 		System.out.printf("Notificando %s atravéz do email: %s\n->%s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
 }
--- artfood-api/src/main/java/com/art/artfood/di/service/AtivacaoClienteService.java
+++ artfood-api/src/main/java/com/art/artfood/di/service/AtivacaoClienteService.java
@@ -1,22 +1,33 @@
 package com.art.artfood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.art.artfood.di.modelo.Cliente;
 import com.art.artfood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 	
+	@Autowired
 	private Notificador notificador;
 
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("Ativação cliente: " + notificador);
-	}
-	
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//		
+//		System.out.println("Ativação cliente: " + notificador);
+//	}
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
 		this.notificador.notificar(cliente, "Seu cadastro está ativo!");
 	}
+	
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+	
 }
