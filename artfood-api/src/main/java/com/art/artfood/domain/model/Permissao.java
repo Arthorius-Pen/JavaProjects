6f56fa33906bff8818376132bb895ad29cf7a5d6
Arthorius-Pen <utherpendragon@tutanota.com>
2022-02-16 20:43:36 -0300

    Created Permissao model

---
+++ artfood-api/src/main/java/com/art/artfood/domain/model/Permissao.java
@@ -0,0 +1,63 @@
+package com.art.artfood.domain.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Permissao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Permissao other = (Permissao) obj;
+		return Objects.equals(id, other.id);
+	}
+}
