9d2bf3482b57f404e0c8c3fb95443633428645b1
Arthorius-Pen <utherpendragon@tutanota.com>
2022-02-16 21:45:10 -0300

    Created estado repository implamentation

---
+++ artfood-api/src/main/java/com/art/artfood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.art.artfood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.art.artfood.domain.model.Estado;
+import com.art.artfood.domain.repository.EstadoRepository;
+
+@Component
+public class EstadoRepositoryImpl implements EstadoRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@Override
+	public List<Estado> listar() {
+		return manager.createQuery("from Cozinha", Estado.class).getResultList();
+	}
+
+	@Override
+	public Estado buscar(Long id) {
+		return manager.find(Estado.class, id);
+	}
+	
+	@Transactional
+	@Override
+	public Estado salvar(Estado estado) {
+		return manager.merge(estado);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Estado estado) {
+		estado = this.buscar(estado.getId());
+		manager.remove(estado);		
+	}
+	
+}
