a2ba0f2ee7ff39867a12270bffeef32ace1e2f18
Arthorius-Pen <utherpendragon@tutanota.com>
2022-02-23 21:20:44 -0300

    Created Cozinha and Estado Controllers

---
+++ artfood-api/src/main/java/com/art/artfood/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.art.artfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.art.artfood.domain.model.Cozinha;
+import com.art.artfood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.listar();
+	}
+	
+}
