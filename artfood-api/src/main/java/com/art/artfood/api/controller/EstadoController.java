f2e8962225c05f7321f20c7ac173f21b2a3fc3b1
Arthorius-Pen <utherpendragon@tutanota.com>
2022-02-23 21:44:53 -0300

    Crated Accecpt examples

--- artfood-api/src/main/java/com/art/artfood/api/controller/EstadoController.java
+++ artfood-api/src/main/java/com/art/artfood/api/controller/EstadoController.java
@@ -3,6 +3,7 @@ package com.art.artfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import com.art.artfood.domain.model.Estado;
 import com.art.artfood.domain.repository.EstadoRepository;
 
 @RestController
-@RequestMapping("/estados")
+@RequestMapping(value = "/estados", produces = MediaType.APPLICATION_XML_VALUE)
 public class EstadoController {
 	
 	@Autowired
