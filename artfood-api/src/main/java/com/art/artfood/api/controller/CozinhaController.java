e108ba67be32158795121900e69f8eb37a79203a
Arthorius-Pen <utherpendragon@tutanota.com>
2022-03-07 19:51:55 -0300

    Using POST method

--- artfood-api/src/main/java/com/art/artfood/api/controller/CozinhaController.java
+++ artfood-api/src/main/java/com/art/artfood/api/controller/CozinhaController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,14 @@ public class CozinhaController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha) {
+		Cozinha cozinhaSalva = cozinhaRepository.salvar(cozinha);
+		
+		return ResponseEntity.status(201).body(cozinhaSalva);
+		
 	}
 	
 }
