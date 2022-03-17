a8db7d8b814f9e7ec26a3755374f910f03b32f81
Arthorius-Pen <utherpendragon@tutanota.com>
2022-03-16 21:07:29 -0300

    Created post to restaurante resource

--- artfood-api/src/main/java/com/art/artfood/api/controller/RestauranteController.java
+++ artfood-api/src/main/java/com/art/artfood/api/controller/RestauranteController.java
@@ -3,14 +3,19 @@ package com.art.artfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.art.artfood.domain.exeption.EntidadeNaoEncontradaExeption;
 import com.art.artfood.domain.model.Restaurante;
 import com.art.artfood.domain.repository.RestauranteRepository;
+import com.art.artfood.domain.service.CadastroRestaurantesService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -18,6 +23,9 @@ public class RestauranteController {
 	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CadastroRestaurantesService cadastroRestaurante;
 	
 	@GetMapping()
 	public List<Restaurante> listar() {
@@ -33,5 +41,17 @@ public class RestauranteController {
 		}
 			 
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> salvar(@RequestBody Restaurante restaurante) {
+		try {			
+			restaurante = cadastroRestaurante.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+		} catch (EntidadeNaoEncontradaExeption e) {
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
 	}
 }
--- artfood-api/src/main/java/com/art/artfood/domain/service/CadastroRestaurantesService.java
+++ artfood-api/src/main/java/com/art/artfood/domain/service/CadastroRestaurantesService.java
@@ -1,16 +1,31 @@
 package com.art.artfood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.art.artfood.domain.exeption.EntidadeNaoEncontradaExeption;
 import com.art.artfood.domain.model.Restaurante;
+import com.art.artfood.domain.repository.CozinhaRepository;
 import com.art.artfood.domain.repository.RestauranteRepository;
 
+@Service
 public class CadastroRestaurantesService {
 	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
 	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		
+		if (cozinhaRepository.buscar(cozinhaId) == null) {
+			throw new EntidadeNaoEncontradaExeption(
+					String.format("Não existe cozinha com o id %d", cozinhaId));
+		}
+		
 		return restauranteRepository.salvar(restaurante);
+		
 	}
 }
