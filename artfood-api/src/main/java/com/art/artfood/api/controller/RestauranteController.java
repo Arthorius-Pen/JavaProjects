463c83e2a8d3a3e1323c7de62fbbcdf8b32f4bde
Arthorius-Pen <utherpendragon@tutanota.com>
2022-03-16 21:31:05 -0300

    Created Put restaurante resource

--- artfood-api/src/main/java/com/art/artfood/api/controller/RestauranteController.java
+++ artfood-api/src/main/java/com/art/artfood/api/controller/RestauranteController.java
@@ -2,12 +2,14 @@ package com.art.artfood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +51,35 @@ public class RestauranteController {
 			restaurante = cadastroRestaurante.salvar(restaurante);
 			
 			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+			
 		} catch (EntidadeNaoEncontradaExeption e) {
+			
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> autualizar(
+				@PathVariable("restauranteId") Long restauranteId,
+				@RequestBody Restaurante restaurante
+			){
+		
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if(restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+		
+		try {			
+			restaurante = cadastroRestaurante.salvar(restauranteAtual);
+			
+			return ResponseEntity.ok(restauranteAtual);
+			
+		} catch (EntidadeNaoEncontradaExeption e) {
+			
 			return ResponseEntity.badRequest()
 					.body(e.getMessage());
 		}
--- artfood-api/src/main/java/com/art/artfood/domain/service/CadastroRestaurantesService.java
+++ artfood-api/src/main/java/com/art/artfood/domain/service/CadastroRestaurantesService.java
@@ -28,4 +28,5 @@ public class CadastroRestaurantesService {
 		return restauranteRepository.salvar(restaurante);
 		
 	}
+	
 }
